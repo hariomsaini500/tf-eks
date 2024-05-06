7eba62528b4161891b68d65bd9a3f2c0d00b9cb8
hariomsaini500 <101454564+hariomsaini500@users.noreply.github.com>
2024-05-07 01:35:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -281,33 +281,33 @@ module "karpenter" {
 # Supporting Resources
 ################################################################################
 
-module "vpc" {
-  source  = "./modules/vpc"
-  #version = "~> 5.0"
+#module "vpc" {
+# source  = "./modules/vpc"
+#  #version = "~> 5.0"
 
-  name = local.name
-  cidr = local.vpc_cidr
+#  name = local.name
+#  cidr = local.vpc_cidr
 
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
-  intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
+#  azs             = local.azs
+#  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+#  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+#  intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+#  enable_nat_gateway = true
+#  single_nat_gateway = true
 
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
+#  public_subnet_tags = {
+#    "kubernetes.io/role/elb" = 1
+#  }
 
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-    # Tags subnets for Karpenter auto-discovery
-    "karpenter.sh/discovery" = local.name
-  }
+#  private_subnet_tags = {
+#    "kubernetes.io/role/internal-elb" = 1
+#    # Tags subnets for Karpenter auto-discovery
+#    "karpenter.sh/discovery" = local.name
+#  }
 
-  tags = local.tags
-}
+#  tags = local.tags
+# }
 ################################################################################
 # Argo helm
 ################################################################################
